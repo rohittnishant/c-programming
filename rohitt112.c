7fe3e000e6caa844efc15937ddf9245ac8336edb
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-12 15:55:38 +0530

    Create rohitt112.c

---
+++ rohitt112.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter A:");
+scanf("%d",&a);
+a=a*2;
+printf("\nThe nearest Number is:%d",a);
+return 0;
+getch();
+}
